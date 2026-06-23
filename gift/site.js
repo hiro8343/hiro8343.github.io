9471291eb623b5816d58709ba8016243ac849467
hiro8343 <hiro.mosmos.jp@gmail.com>
2026-06-23 12:21:07 +0900

    giftサイト更新・events.html追加

--- gift/site.js
+++ gift/site.js
@@ -89,6 +89,7 @@
           <h4>つながる</h4>
           <div class="footer-links">
             <a href="book.html#yoyaku">書籍を予約する</a>
+            <a href="events.html">イベント情報</a>
             <a href="reviews.html">読んだ方の感想</a>
             <a href="learn.html">動画で学ぶ</a>
             <a href="support.html">循環に加わる</a>
