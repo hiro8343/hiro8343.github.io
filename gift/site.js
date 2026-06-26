7d39dc386fed3ed3d1b92fb35ef0118f8cd83228
hiro8343 <hiro.mosmos.jp@gmail.com>
2026-06-26 20:04:11 +0900

    course.html追加・learn.html・site.js更新

--- gift/site.js
+++ gift/site.js
@@ -90,6 +90,7 @@
           <div class="footer-links">
             <a href="https://amzn.to/43Rurig" target="_blank" rel="noopener">書籍を予約する</a>
             <a href="events.html">イベント情報</a>
+            <a href="course.html">ギフトの循環講座</a>
             <a href="reviews.html">読んだ方の感想</a>
             <a href="learn.html">動画で学ぶ</a>
             <a href="support.html">循環に加わる</a>
